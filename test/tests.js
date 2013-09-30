7a6182f42c163e8dedef2602dcf7606a77dadc16
Ryan Van Etten <ryanve@gmail.com>
2013-09-30 19:45:43 -0400

    Use helper functions to setup tests.

--- test/tests.js
+++ test/tests.js
@@ -9,15 +9,37 @@
       , divs = docElem[byTag]('div')
       , html = scan('html')
       , body = scan('body');
-      
+
     3 > divs.length && aok.error('Tests require 3+ divs.');
     
+    // Use alert if console is unavail.
+    aok.prototype.express.force = true;
+
     function isElement() {
         return 1 === this.nodeType;
     }
     
-    // Use alert if console is unavail.
-    aok.prototype.express.force = true;
+    function every(ob, fn, scope) {
+        var l = ob.length, i = 0;
+        while (i < l) if (!fn.call(scope, ob[i], i++, ob)) return false;
+        return true;
+    }
+    
+    /**
+     * @this {Function}
+     * @param {Array} arr [expectedResult, scope, *args]
+     */
+    function complies(arr) {
+        return arr[0] === this.apply(arr[1], arr.slice(2));
+    }
+    
+    /**
+     * @this {Function}
+     * @param {Array} arr [expectedLength, scope, *args]
+     */
+    function tallies(arr) {
+        return arr[0] === this.apply(arr[1], arr.slice(2)).length;
+    }
 
     aok({id:'selectTag', test:function() {
         return scan.qsa('div').length === document[byTag]('div').length;
@@ -28,53 +50,61 @@
     }});
     
     aok({id:'contains', test:function() {
-        return true === scan.contains('str', 's') &&
-            true === scan.contains('str', 'tr') &&
-            false === scan.contains('str', 'a') &&
-            true === scan.contains(docElem, docElem[byTag]('*')[0]) &&
-            false === scan.contains(docElem[byTag]('*')[0], docElem) &&
-            false === scan.contains(docElem, docElem) &&
-            true === scan.contains([0, 1], 1) &&
-            false === scan.contains([0, 1], 2) &&
-            false === scan.contains({}, 1);
+        return every([
+            [true, null, 'str', 's'],
+            [true, null, 'str', 'tr'],
+            [false, null, 'str', 'a'],
+            [true, null, [0, 1], 1],
+            [false, null, [0, 1], 2],
+            [false, null, {}, 1],
+            [true, null, docElem, docElem[byTag]('*')[0]],
+            [false, null, docElem[byTag]('*')[0], docElem],
+            [false, null, docElem, docElem]
+        ], complies, scan.contains);
     }});
     
     aok({id:'fnFindSelect', test:function() {
         var find = scan.fn.find;
-        if (1 !== find.call(html, 'body').length) return false; 
-        if (0 !== find.call(body, 'html').length) return false;
-        // QSA effectively queries top-down and then filters by those contained
-        if (hasQsa != find.call(html, 'html body').length) return false;
-        return scan('*').length > find.call(body, '*').length;
+        return scan('*').length > find.call(body, '*').length && every([
+            [1, html, 'body'],
+            [0, body, 'html'],
+            // QSA effectively queries top-down and then filters by those contained
+            [+hasQsa, html, 'html body']
+        ], tallies, find);
     }});
     
     aok({id:'fnFindObject', test:function() {
-        var find = scan.fn.find, main = scan('main'), div = scan('div');
-        if (1 !== find.call(html, body[0]).length) return false;
-        if (0 !== find.call(body, html[0]).length) return false;
-        if (1 !== find.call(html, body).length) return false;
-        if (0 !== find.call(body, html).length) return false;
-        if (2 !== find.call(html, [body[0], body[0]]).length) return false;
-        return find.call(main, div).length <= div.length;
+        var find = scan.fn.find, main = scan('main');
+        return find.call(main, divs).length <= divs.length && every([
+            [1, html, body[0]],
+            [0, body, html[0]],
+            [1, html, body],
+            [0, body, html],
+            [2, html, [body[0], body[0]]]
+        ], tallies, find);
     }});
     
     aok({id:'fnFilter', test:function() {
-        var fn = scan.fn.filter;
-        if (divs.length !== fn.call(divs, 'div').length) return false;
-        if (divs.length !== fn.call(divs, isElement).length) return false;
-        if (1 !== fn.call(divs, divs[0]).length) return false;
-        if (2 !== fn.call(divs, [divs[0], divs[1]]).length) return false;
-        if (1 !== fn.call(divs, [divs[0], divs[0]]).length) return false;
-        return 0 === fn.call(divs).length && 0 === fn.call(divs, []).length;
+        return every([
+            [0, divs],
+            [0, divs, []],
+            [1, divs, divs[0]],
+            [1, divs, [divs[0], divs[0]]],
+            [2, divs, [divs[0], divs[1]]],
+            [divs.length, divs, isElement],
+            [divs.length, divs, 'div'],
+        ], tallies, scan.fn.filter);
     }});
     
     aok({id:'fnNot', test:function() {
-        var fn = scan.fn.not, size = divs.length;
-        if (0 !== fn.call(divs, 'div').length) return false;
-        if (0 !== fn.call(divs, isElement).length) return false;
-        if ((size-1) !== fn.call(divs, divs[0]).length) return false;
-        if ((size-2) !== fn.call(divs, [divs[0], divs[1]]).length) return false;
-        if ((size-1) !== fn.call(divs, [divs[0], divs[0]]).length) return false;
-        return size === fn.call(divs).length && size === fn.call(divs, []).length;
+        return every([
+            [divs.length, divs],
+            [divs.length, divs, []],
+            [divs.length-1, divs, divs[0]],
+            [divs.length-1, divs, [divs[0], divs[0]]],
+            [divs.length-2, divs, [divs[0], divs[1]]],
+            [0, divs, isElement],
+            [0, divs, 'div'],
+        ], tallies, scan.fn.not);
     }});
 }(this, document));
