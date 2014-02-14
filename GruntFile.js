dddd12a9a85e118faa182c05a4a34ced784bf45a
Ryan Van Etten <ryanve@gmail.com>
2014-02-14 05:31:12 -0500

    Use gerneric index.js as filename in sub folders.

--- GruntFile.js
+++ GruntFile.js
@@ -1,6 +1,19 @@
 module.exports = function(grunt) {
+  var _ = grunt.util._,
+    pkg = grunt.file.readJSON('package.json'),
+    path = require('path'),
+    from = 'src/',
+    main = pkg.main && path.basename(pkg.main) || 'index.js',
+    source = [_.find([from + 'index.js', from + main, from + pkg.name], function(v) {
+      return this.existsSync(v);
+    }, require('fs'))],
+    holder = (function(who) {
+      return typeof who == 'string' ? who.split(/\s+</)[0].trim() : who && who.name || '';
+    }(pkg.author));
+
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: pkg,
+    aok: { test: ['./test'] },
     jshint: {
       // gruntjs.com/configuring-tasks#globbing-patterns
       // **/** matches in current and sub dirs
@@ -9,13 +22,13 @@ module.exports = function(grunt) {
       dir: ['*.js'], // current dir
       src: ['src/'],
       test: ['test/'],
-      grunt: ['GruntFile.js'],
-      build: ['<%= pkg.name %>.js'],
+      grunt: [path.basename(__filename)],
+      build: [main],
       options: {
         ignores: ['**/**/node_modules/', '**/**/vendor/', '**/**.min.js'],
-        expr:true, sub:true, supernew:true, debug:true, node:true, 
-        boss:true, devel:true, evil:true, laxcomma:true, eqnull:true, 
-        undef:true, unused:true, browser:true, jquery:true, maxerr:10
+        debug:true, expr:true, sub:true, boss:true, supernew:true, node:true, 
+        undef:true, unused:true, devel:true, evil:true, laxcomma:true, eqnull:true, 
+        browser:true, globals:{ender:true, define:true}, jquery:true, maxerr:10
       }
     },
     concat: {
@@ -24,14 +37,12 @@ module.exports = function(grunt) {
           '/*!',
           ' * <%= pkg.name %> <%= pkg.version %>+<%= grunt.template.today("UTC:yyyymmddHHMM") %>',
           ' * <%= pkg.homepage %>',
-          ' * MIT License 2013 <%= pkg.author %>',
+          ' * MIT License <%= grunt.template.today("UTC:yyyy") %> ' + holder,
           ' */\n\n'
         ].join('\n')
       },
       build: {
-        files: {
-          '<%= pkg.name %>.js': ['src/<%= pkg.name %>.js']
-        }
+        files: _.object([main], [source])
       }
     },
     uglify: {
@@ -40,12 +51,12 @@ module.exports = function(grunt) {
         preserveComments: 'some'
       },
       build: {
-        files: {
-          '<%= pkg.name %>.min.js': ['<%= pkg.name %>.js']
-        }
+        files: _.object([main.replace(/\.js$/i, '.min.js')], [main])
       }
     }
   });
+
+  grunt.loadNpmTasks('aok');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
